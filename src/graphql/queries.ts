aa37cdf56f322340586acdd3ed0eccd8e1fe315e
Anderson <andersoninfonet@gmail.com>
2021-03-20 02:15:21 -0300

    add page of places

--- src/graphql/queries.ts
+++ src/graphql/queries.ts
@@ -25,3 +25,47 @@ export const GET_PAGE_BY_SLUG = gql`
     }
   }
 `
+
+export const GET_PLACES = gql`
+  query getPlaces {
+    places {
+      id
+      name
+      slug
+      location {
+        latitude
+        longitude
+      }
+      description {
+        html
+      }
+      gallery {
+        url
+        width
+        height
+      }
+    }
+  }
+`
+
+export const GET_PLACE_BY_SLUG = gql`
+  query getPlaceBySlug($slug: String!) {
+    place(where: { slug: $slug }) {
+      id
+      name
+      slug
+      location {
+        latitude
+        longitude
+      }
+      description {
+        html
+      }
+      gallery {
+        url
+        width
+        height
+      }
+    }
+  }
+`
---
+++ src/templates/Places/index.tsx
@@ -0,0 +1,43 @@
+import LinkWrapper from 'components/LinkWrapper'
+import { CloseOutline } from '@styled-icons/evaicons-outline/CloseOutline'
+import * as S from './styles'
+
+export interface ImageProps {
+  url: string
+  width: number
+  height: number
+}
+
+export interface PlaceTemplateProps {
+  place: {
+    slug: string
+    name: string
+    description: {
+      html: string
+    }
+    gallery: ImageProps[]
+  }
+}
+
+const PlaceTemplate = ({ place }: PlaceTemplateProps) => (
+  <>
+    <LinkWrapper href="/">
+      <CloseOutline size={32} aria-label="Go to Map" />
+    </LinkWrapper>
+    <S.Wrapper>
+      <S.Container>
+        <S.Heading>{place.name}</S.Heading>
+
+        <S.Body dangerouslySetInnerHTML={{ __html: place.description?.html }} />
+
+        <S.Gallery>
+          {place.gallery.map((image, index) => (
+            <img src={image.url} alt={place.name} key={`image_${index}`} />
+          ))}
+        </S.Gallery>
+      </S.Container>
+    </S.Wrapper>
+  </>
+)
+
+export default PlaceTemplate
